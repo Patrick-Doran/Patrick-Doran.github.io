2afcd0d8d496138c57ee1f1d48330bd327b61d69
Patrick-Doran <wombatpatrick@gmail.com>
2023-03-23 16:54:22 -0700

    nav init

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, {useEffect} from "react";
+
+const Header = () => {
+    return(
+        <nav>
+            <p className="logo">Patrick Doran //</p>
+            <a href="#home">Home</a>
+            <a href="#about">About</a>
+        </nav>
+    );
+};
+
+export default Header;
