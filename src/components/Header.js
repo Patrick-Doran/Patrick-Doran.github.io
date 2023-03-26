24d191f0b275b1c4a0af9a798d047fd9cdb33ba1
Patrick-Doran <wombatpatrick@gmail.com>
2023-03-26 12:58:04 -0700

    nav elements, fa install

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,6 @@
-import React, {useEffect} from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 const Header = () => {
     return(
@@ -6,6 +8,8 @@ const Header = () => {
             <p className="logo">Patrick Doran //</p>
             <a href="#home">Home</a>
             <a href="#about">About</a>
+            <a href="https://github.com/Patrick-Doran"><FontAwesomeIcon icon={faGithub} size="2x" inverse/></a>
+            <a href="https://www.linkedin.com/in/patrick-doran/"><FontAwesomeIcon icon={faLinkedin} size="2x" inverse/></a>
         </nav>
     );
 };
