e4a6c928044b4b728f388bcaf1a5f0384cfafd0e
Patrick-Doran <wombatpatrick@gmail.com>
2023-03-28 14:03:38 -0700

    create footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+    return(
+        <Footer>
+            <p>&copy Patrick Doran 2023</p>
+        </Footer>
+    );
+};
+
+export default Footer;
