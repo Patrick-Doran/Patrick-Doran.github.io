391076d0b942fedb1546bc0af1ba959a768b82ce
Patrick-Doran <wombatpatrick@gmail.com>
2023-03-28 15:17:49 -0700

    footer fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Footer = () => {
     return(
-        <Footer>
-            <p>&copy Patrick Doran 2023</p>
-        </Footer>
+        <footer>
+            <p>&copy; Patrick Doran 2023</p>
+        </footer>
     );
 };
 
