a2fabaf3e90c57db5083c99e43a27d72a8ad742b
Patrick-Doran <wombatpatrick@gmail.com>
2024-01-27 17:58:52 -0800

    creation of personal

--- src/components/Personal.js
+++ src/components/Personal.js
@@ -1,18 +1,11 @@
 import React from "react";
-import karmere from "../assets/projects/karmere.png"
-import cloverdale from "../assets/projects/cloverdale.png"
-import shadyLane from "../assets/projects/shadyLane.png"
 
 const Personal = () => {
     return(
-        <div className="projects">
-            <div className="projectImages">
-                <a href="https://www.karmere.com/" target="_blank"><img src={karmere} alt="description" /></a>
-                <a href="https://cloverdalechamber.com/experience-cloverdale/" target="_blank"><img src={cloverdale} alt="description" /></a>
-                <a href="https://www.shadylanecellars.com/brio/" target="_blank"><img src={shadyLane} alt="description" /></a>
-            </div>
+        <div>
+            <p>text</p>
         </div>
     );
-}
+};
 
 export default Personal;
